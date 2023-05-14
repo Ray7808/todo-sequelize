71340a04853b3214aa5ae6e8f8c1892d9c988320
Ray7808 <yuwang7808@gmail.com>
2023-05-14 22:32:46 +0800

    feat: add .env.example and flash

--- app.js
+++ app.js
@@ -1,24 +1,24 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
 const methodOverride = require('method-override')
-const bcrypt = require('bcryptjs')
 const session = require('express-session')
-const passport = require('passport') // 把 Passport 套件本身載入進來 ->因為登入驗證要用Passport的authenticate套件，因此還是要載入這個
 const usePassport = require('./config/passport') // 載入設定檔，要寫在 express-session 以後
-const routes = require('./routes')
+const flash = require('connect-flash')
 
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
+
+const routes = require('./routes')
 const app = express()
-const PORT = 3000
-const db = require('./models')
-const Todo = db.Todo
-const User = db.User
+const PORT = process.env.PORT
 
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
 
 app.use(
   session({
-    secret: 'ThisIsMySecret',
+    secret: process.env.SESSION_SECRET,
     resave: false,
     saveUninitialized: true,
   })
@@ -27,6 +27,15 @@ app.use(express.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
 // 呼叫 Passport 函式並傳入 app，這條要寫在路由之前
 usePassport(app)
+
+app.use(flash())
+app.use((req, res, next) => {
+  res.locals.isAuthenticated = req.isAuthenticated()
+  res.locals.user = req.user
+  res.locals.success_msg = req.flash('success_msg')
+  res.locals.warning_msg = req.flash('warning_msg')
+  next()
+})
 
 app.use(routes)
 
